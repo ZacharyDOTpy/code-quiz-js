6895cc648bb455a0ab511d5b146962b76eaf0175
Zach <zach.Brewer.1995@gmail.com>
2023-11-11 14:16:02 -0500

    added top bar & end section | added event listeners to end screen buttons and highscore button

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,37 @@
 // housekeeping
 var startScreen = document.querySelector('.start');
 var quizScreen = document.querySelector('.quiz');
+var endScreen = document.querySelector('.end');
 var scoreScreen = document.querySelector('.score');
 var startButton = document.querySelector('#start-btn');
+var scoreButton = document.querySelector('#score-btn');
 
-// added funtions for init|startScreen|quizScreen|scoreScreen
+// added funtions for init|startScreen|quizScreen|endScreen|scoreScreen
 function showStart() {
     startScreen.style.display = null;
     quizScreen.style.display = "none";
+    endScreen.style.display = "none";
     scoreScreen.style.display = "none";
 }
 
 function showQuiz() {
     startScreen.style.display = "none";
     quizScreen.style.display = null;
+    endScreen.style.display = "none";
+    scoreScreen.style.display = "none";
+}
+
+function showEnd() {
+    startScreen.style.display = "none";
+    quizScreen.style.display = "none";
+    endScreen.style.display = null;
     scoreScreen.style.display = "none";
 }
 
 function showScore() {
     startScreen.style.display = "none";
     quizScreen.style.display = "none";
+    endScreen.style.display = "none";
     scoreScreen.style.display = null;
 }
 
@@ -31,8 +43,18 @@ startButton.addEventListener('click', function(event) {
 // added event listener to quizScreen buttons
 quizScreen.addEventListener('click', function(event) {
     if (event.target.matches('button')) {
+        showEnd();
+    }
+});
+// added event listener to endScreen buttons
+endScreen.addEventListener('click', function(event) {
+    if (event.target.matches('button')) {
         showScore();
     }
+});
+// added event listener to highscore button
+scoreButton.addEventListener('click', function(event) {
+    showScore();
 });
 
 function init() {
