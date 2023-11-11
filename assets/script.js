83f54b63cc9225f88e3bb4fa6287fd16c4d32275
Zach <zach.Brewer.1995@gmail.com>
2023-11-11 15:19:32 -0500

    added questions object array

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,29 @@ var startScreen = document.querySelector('.start');
 var quizScreen = document.querySelector('.quiz');
 var endScreen = document.querySelector('.end');
 var scoreScreen = document.querySelector('.score');
+
 var startButton = document.querySelector('#start-btn');
 var scoreButton = document.querySelector('#score-btn');
+var submitButton = document.querySelector('#submit-btn');
+
+// added questions object array
+var questions = [
+    {
+        question: "How many moons does Jupiter have?",
+        choices: ["60", "63", "95", "12"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which planet is closest to the Sun?",
+        choices: ["Mercury", "Neptune", "Mars", "Venus"],
+        correctAnswer: 0
+    },
+    {
+        question: "How many gas giants are in our solar system?",
+        choices: ["2", "4", "3", "0"],
+        correctAnswer: 1
+    },
+]
 
 // added funtions for init|startScreen|quizScreen|endScreen|scoreScreen
 function showStart() {
@@ -46,12 +67,12 @@ quizScreen.addEventListener('click', function(event) {
         showEnd();
     }
 });
+
 // added event listener to endScreen buttons
-endScreen.addEventListener('click', function(event) {
-    if (event.target.matches('button')) {
-        showScore();
-    }
+submitButton.addEventListener('click', function(event) {
+    showScore();
 });
+
 // added event listener to highscore button
 scoreButton.addEventListener('click', function(event) {
     showScore();
