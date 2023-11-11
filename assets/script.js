28e253e14d8f15b13f59f6b776c92c704ff91a9a
Zach <zach.Brewer.1995@gmail.com>
2023-11-10 19:26:41 -0500

    added housekeeping and functions for init|startScreen|quizScreen|scoreScreen

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
-console.log('hello');
+// housekeeping
+var startScreen = document.querySelector('.start');
+var quizScreen = document.querySelector('.quiz');
+var scoreScreen = document.querySelector('.score');
+
+// added funtions for init|startScreen|quizScreen|scoreScreen
+function showStart() {
+    startScreen.style.display = null;
+    quizScreen.style.display = "none";
+    scoreScreen.style.display = "none";
+}
+
+function showQuiz() {
+    startScreen.style.display = "none";
+    quizScreen.style.display = null;
+    scoreScreen.style.display = "none";
+}
+
+function showScore() {
+    startScreen.style.display = "none";
+    quizScreen.style.display = "none";
+    scoreScreen.style.display = null;
+}
+
+function init() {
+    showStart();
+}
+
+init();
