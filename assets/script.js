301d72eb39e74717457e2abb38907cca3a699338
Zach <zach.Brewer.1995@gmail.com>
2024-01-27 11:27:15 -0500

    added more event listeners and local storage, reformatted document w/ prettier

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,50 @@
+// housekeeeping
 var viewScores = document.querySelector('#view-score-screen');
-var displayScore = document.querySelector('#score');
+var displayScores = document.querySelector('#score');
 var feedback = document.querySelector('#feedback');
 
-var strtScreen = document.querySelector('.start');
-var strtBtn = document.querySelector('#start-btn');
+var startScreen = document.querySelector('.start');
+var startBtn = document.querySelector('#start-btn');
 
 var quizScreen = document.querySelector('.quiz');
-var questionsArea = document.querySelector('#questions');
+var questionsContainer = document.querySelector('#questions');
 
 var endScreen = document.querySelector('.end');
-var initialsForm = document.querySelector('#initials');
+var initialsField = document.querySelector('#initials');
 var submitBtn = document.querySelector('#submit-btn');
 
-var scoreScreen = document.querySelector('.score');
-var scoreList = document.querySelector('#score-list');
+var scoresScreen = document.querySelector('.score');
+var scoresList = document.querySelector('#score-list');
 var clearBtn = document.querySelector('#clear-scores');
 var retryBtn = document.querySelector('#retry-btn');
 
+// questions array
 var questions = [
   {
-    "question": "How many moon does Jupiter have?",
-    "answers": ["60", "63", "95", "12"],
-    "correctAnswer": 2
+    question: 'How many moon does Jupiter have?',
+    answers: ['60', '63', '95', '12'],
+    correctAnswer: 2,
   },
   {
-    "question": "Which planet is closest to the Sun?",
-    "answers": ["Mercury", "Neptune", "Mars", "Venus"],
-    "correctAnswer": 0
+    question: 'Which planet is closest to the Sun?',
+    answers: ['Mercury', 'Neptune', 'Mars', 'Venus'],
+    correctAnswer: 0,
   },
   {
-    "question": "How many gas giants are in our solar system?",
-    "answers": ["2", "4", "3", "0"],
-    "correctAnswer": 1
+    question: 'How many gas giants are in our solar system?',
+    answers: ['2', '4', '3', '0'],
+    correctAnswer: 1,
   },
   {
-    "question": "How many minutes does it take light to travel from the Sun to Earth?",
-    "answers": ["0", "12", "7", "8"],
-    "correctAnswer": 3
+    question:
+      'How many minutes does it take light to travel from the Sun to Earth?',
+    answers: ['0', '12', '7', '8'],
+    correctAnswer: 3,
   },
   {
-    "question": "How many planets have rings in our solar system?",
-    "answers": ["2", "4", "1", "0"],
-    "correctAnswer": 1
+    question: 'How many planets have rings in our solar system?',
+    answers: ['2', '4', '1', '0'],
+    correctAnswer: 1,
   },
 ];
 var index = 0;
@@ -50,113 +53,161 @@ var score = 100;
 var highscores = [];
 var timer;
 
-// function to display each screen of the program
+// function to show each screen of program
 function show(screen) {
-  strtScreen.style.display = "none";
-  quizScreen.style.display = "none";
-  endScreen.style.display = "none";
-  scoreScreen.style.display = "none";
+  
+  startScreen.style.display = 'none';
+  quizScreen.style.display = 'none';
+  endScreen.style.display = 'none';
+  scoresScreen.style.display = 'none';
 
   document.querySelector('.' + screen).style.display = null;
-};
+}
 
-// start quiz function
-function quizStart() {
-  // resets variables
+// function to start quiz and reset variables
+function startQuiz() {
+  
   index = 0;
   score = 100;
-  scoreScreen.textContent = 'Score: ' + score;
+  displayScores.textContent = 'Score: ' + score;
 
-  // shows quiz screen with question and starts timer
+  // shows quiz screen & displays first question
   show('quiz');
   displayQuestion();
-  timer = setInterval(decrementScore, 1000, 1);
-};
+  timer = setInterval(decreaseScore, 1000, 1);
+}
 
-// function to display each question
+// function to display each question in questions array
 function displayQuestion() {
-  questionsArea.innerHTML = null;
+  
+  questionsContainer.innerHTML = null;
 
-  // appends question from array to questions div
+  // appends questions from questions array to container
   var questionElement = document.createElement('p');
-  questionElement.textContent = questions[index].question
-  questionsArea.appendChild(questionElement);
+  questionElement.textContent = questions[index].question;
+  questionsContainer.appendChild(questionElement);
 
-  // appends answers from array to questions div
-  var answer = questions[index].answers;
+  // appends answers from questions array to container
+  var answers = questions[index].answers;
   for (var i = 0; i < answers.length; i++) {
     var answerElement = document.createElement('button');
-    answerElement.textContent = answer[i];
+    answerElement.textContent = answers[i];
     answerElement.setAttribute('style', 'display: block;');
-    questionsArea.appendChild(answerElement);
-  };
-};
+    questionsContainer.appendChild(answerElement);
+  }
+}
 
 // function to decrease score based on timer
-function decrementScore(value) {
-  score -= value;
-  scoreScreen.textContent = 'Score: ' + score;
+function decreaseScore(amount) {
+  score -= amount;
+  displayScores.textContent = 'Score: ' + score;
   if (score <= 0) {
-    // clears timer and shows quiz end screen
     clearInterval(timer);
     show('end');
-  };
-};
+  }
+}
 
-// function the show scores screen
+// function to display highscores
 function renderScores() {
-  scoreList.innerHTML = '';
+  
+  scoresList.innerHTML = '';
 
-  // sorts scores from high to low
-  highscores = highscores.sort(function(a, b){return b.score - a.score})
-  // renders new scores with initials
+  // displays scores from high to low
+  highscores = highscores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+  // creates list of scores & appends them
   for (var i = 0; i < highscores.length; i++) {
     var highscore = highscores[i];
 
-    var list = document.createElement('li');
-    list.textContent = highscore.initials + ' - ' + highscore.score;
-    list.setAttribute('data-index', i);
+    var li = document.createElement('li');
+    li.textContent = highscore.initials + ' - ' + highscore.score;
+    li.setAttribute('data-index', i);
 
-    scoreList.appendChild(list);
-  };
-};
-
-// event listener for scores button & stops timer
-viewScores.addEventListener('click', function() {
+    scoresList.appendChild(li);
+  }
+}
+// event listener for highscores button
+viewScores.addEventListener('click', function () {
+  
   if (timer) {
     clearInterval(timer);
   }
-  
-  show('highscores');
-})
-
-// event listener for start quiz button
-strtBtn.addEventListener('click', function() {
-  quizStart();
-})
-
+  show('score');
+});
+// event listener for start button
+startBtn.addEventListener('click', function () {
+  startQuiz();
+});
 // event listener for answer buttons
-quizScreen.addEventListener('click', function(event) {
+quizScreen.addEventListener('click', function (event) {
   if (event.target.matches('button')) {
     
-    if (event.target.textContent != questions[index]['answers'][questions[index]['correctAnswer']]) {
-      decrementScore(15);
+    if (
+      event.target.textContent !=
+      questions[index]['answers'][questions[index]['correctAnswer']]
+    ) {
+      // decreases score if wrong answer
+      decreaseScore(15);
       feedback.textContent = 'INCORRECT';
-      setTimeout(() => {feedback.textContent = ''}, 1000);
+      setTimeout(() => {
+        feedback.textContent = '';
+      }, 1000);
     } else {
       feedback.textContent = 'CORRECT';
-      setTimeout(() => {feedback.textContent = ''}, 1000);
-    };
+      setTimeout(() => {
+        feedback.textContent = '';
+      }, 1000);
+    }
 
-    // displays the next question if there is one
+    // displays next question
     index++;
     if (questions[index]) {
       displayQuestion();
       return;
-    };
+    }
 
-    // stops timer and shows end screen if no more questions
+    // clears timer and shows end screen if no more questions
     clearInterval(timer);
     show('end');
-  };
+  }
 });
+// event listener for submit button
+submitBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  
+  highscores.push({
+    initials: initialsField.value,
+    score: score,
+  });
+
+  
+  initialsField.value = '';
+
+  
+  localStorage.setItem('score', JSON.stringify(highscores));
+  show('score');
+  renderScores();
+});
+// event listener for clear scores button
+clearBtn.addEventListener('click', function () {
+  highscores = [];
+  localStorage.removeItem('score');
+  renderScores();
+});
+// event listener for retry button
+retryBtn.addEventListener('click', function () {
+  show('start');
+});
+
+function init() {
+  var storedHighscores = JSON.parse(localStorage.getItem('scores'));
+  if (storedHighscores !== null) {
+    highscores = storedHighscores;
+  }
+
+  show('start');
+}
+
+init();
